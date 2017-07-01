4e30330744d5cea0639fba8441a2dd027574a2ff
Homer Cadena <homer.keid@gmail.com>
2017-07-01 17:26:22 +0800

    Assignment-1
    First javascripting assignment.

---
+++ Cadena/javascripting/function-arguments.js
@@ -0,0 +1,5 @@
+function math (hindi, ako, susuko)
+{
+  return (ako * susuko) + hindi
+}
+console.log(math(53, 61, 67))
---
+++ Cadena/javascripting/looping-through-arrays.js
@@ -0,0 +1,7 @@
+var pets = ['cat', 'dog', 'rat']
+for (i=0;i<=2;i++)
+{
+   pets[i]=pets[i] + 's'
+}
+
+console.log(pets)
