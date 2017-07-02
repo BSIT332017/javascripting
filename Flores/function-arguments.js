ea6bf17dfc0cf634a9bfabb09964afe9d52d342e
rachelmaiden <floresmaidenrachel@gmail.com>
2017-07-02 14:30:03 +0800

    assignment

---
+++ Flores/function-arguments.js
@@ -0,0 +1,6 @@
+function math (a, b, c)
+{
+	var ans = a + b*c
+	return ans
+}
+console.log(math(53, 61, 67))
---
+++ Flores/if-statement.js
@@ -0,0 +1,11 @@
+var fruit = 'orange'
+
+if (fruit.length < 5)
+{
+	console.log('The fruit name has five characters or less.')
+}
+
+else
+{
+	console.log ('The fruit name has more than five characters.')
+}
