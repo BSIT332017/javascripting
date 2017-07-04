644a7e8defcfb751a03dfe9c05a4c1633f7a42ba
Jerevon Carreon <jerevoncarreon13@gmail.com>
2017-07-04 08:44:53 -0700

    First Assignment

---
+++ Ganila/if-statement.js
@@ -0,0 +1,12 @@
+var fruit = 'orange';
+
+x=fruit.length
+
+if(x>5)
+{
+	console.log('The fruit name has more than five characters.');
+}
+else
+{
+	console.log('The fruit name has five characters or less.');
+}
---
+++ Ganila/rounding-numbers.js
@@ -0,0 +1,3 @@
+var roundUp = 1.5;
+var x=Math.round(roundUp);
+console.log(x);
