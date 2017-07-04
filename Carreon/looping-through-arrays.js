68df43579158bcdb72d2c8f3b43a2a8b33988fd8
Jerevon Carreon <jerevoncarreon13@gmail.com>
2017-07-04 08:39:00 -0700

    1st Assignment

---
+++ Carreon/looping-through-arrays.js
@@ -0,0 +1,7 @@
+var pets = ['cat', 'dog', 'rat'];
+	var i = 0;	
+	for (i = 0; i < 3; i++)
+	{
+		pets[i] = pets[i] + 's';
+	}
+		console.log(pets)
---
+++ Carreon/variables.js
@@ -0,0 +1,4 @@
+var example = 'some string'; 
+{
+	console.log(example)
+}
